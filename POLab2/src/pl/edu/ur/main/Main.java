f5280df2f416e33a7adfe91bcdaccb7e621a4fc9
asia <asia18117@wp.pl>
2018-03-07 18:20:53 +0100

    L1GR3-7 Zalozenie projektu

--- POLab2/src/pl/edu/ur/main/Main.java
+++ POLab2/src/pl/edu/ur/main/Main.java
@@ -1,5 +1,7 @@
 package pl.edu.ur.main;
 
+import java.util.Scanner;
+
 /**
  *
  * @author marcin
@@ -10,7 +12,91 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+//        int liczba = 100;
+//        
+//        while (liczba != 0) {
+//
+//             liczba = scan.nextInt();
+//            switch (liczba) {
+//                case 1: {
+//                    System.out.println("styczen");
+//                    break;
+//                }
+//                case 2: {
+//                    System.out.println("luty");
+//                    break;
+//                }
+//                case 3: {
+//                    System.out.println("marzec");
+//                    break;
+//                }
+//                case 4: {
+//                    System.out.println("kwiecien");
+//                    break;
+//                }
+//                case 5: {
+//                    System.out.println("maj");
+//                    break;
+//                }
+//                case 6: {
+//                    System.out.println("czerwiec");
+//                    break;
+//                }
+//                case 7: {
+//                    System.out.println("lipiec");
+//                    break;
+//                }
+//                case 8: {
+//                    System.out.println("sierpien");
+//                    break;
+//                }
+//                case 9: {
+//                    System.out.println("wrzesien");
+//                    break;
+//                }
+//                case 10: {
+//                    System.out.println("pazdziernik");
+//                    break;
+//                }
+//                case 11: {
+//                    System.out.println("listopad");
+//                    break;
+//                }
+//                case 12: {
+//                    System.out.println("grudzien");
+//                    break;
+//                }
+//                default: {
+//                    System.out.println("zly numer");
+//                    break;
+//                }
+//            }
+//        }
+
+  
+//for(int a=20;a>=0;a=a-1)
+//{
+//if(a==19 || a==15 || a==9 || a==6 || a==2)
+//{
+//continue;
+//}else System.out.println("zly numer");   
+//}
+
+//int a,b,c,delta;
+//double pierwiastek,d1,d2;
+//a = scan.nextInt();
+//b = scan.nextInt();
+//c = scan.nextInt();
+//delta=(b^2)-4*a*c;
+//pierwiastek=(int)Math.sqrt((int)delta);
+//d1=(b-pierwiastek)/(2*a);
+//d2=(b+pierwiastek)/(2*a);
+//System.out.println("d1="+d1);
+//System.out.println("d2="+d2);
+        
         // TODO zad 3, 4, 6, 7
     }
-    
+
 }
